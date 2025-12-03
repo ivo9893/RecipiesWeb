6b12b8e9bb955763218b58c5f4bbe65a8df97312
Ivko <ivo_stoqnov@mail.bg>
2025-12-03 19:12:44 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <TextField label="Name" variant="outlined" />
+    </div>
+  );
+}
+
+export default App;
